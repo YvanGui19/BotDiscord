5f55e1eaf0bd9de71795f7fc1014f7f9cbeca334
User <User@PC-190>
2024-07-10 20:58:08 +0200

    Ajoutez des fichiers projet.

---
+++ AgentNettoyant/Modules/Commands.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Commands;
+using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentNettoyant.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("Pong !");
+        }
+    }
---
+++ AgentNettoyant/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+
+namespace AgentNettoyant
+{
+    internal class Program
+    {
+        private DiscordSocketClient client;
+        private CommandService commands;
+        private IServiceProvider services;
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        public async Task RunBotAsync()
+        {
+            client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug,
+                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+
+            });
+
+            commands = new CommandService();
+
+            client.Log += Log;
+
+            client.Ready += () =>
+            {
+                Console.WriteLine("Agent prêt à l'emploi !");
+                return Task.CompletedTask;
+            };
+
+            await InstallCommandsAsync();
+
+            await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken", EnvironmentVariableTarget.User));
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        public async Task InstallCommandsAsync()
+        {
+            client.MessageReceived += HandleCommandAsync;
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage pMessage)
+        {
+            var message = (SocketUserMessage)pMessage;
+
+            if (message == null) return;
+
+            int argPos = 0;
+
+            if (!message.HasCharPrefix('!', ref argPos)) return;
+
+            var context = new SocketCommandContext(client, message);
+
+            var result = await commands.ExecuteAsync(context, argPos, services);
+
+            //ERROR
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg.ToString());
+            return Task.CompletedTask;
+        }
+
+    }
+}
