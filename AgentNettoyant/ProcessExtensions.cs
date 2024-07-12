c51473783dc3d963af484068c645bf87f431a067
19 <dixneufcode@gmail.com>
2024-07-12 19:37:44 +0200

    serveurON, serveurOFF, pcOFF

---
+++ AgentNettoyant/ProcessExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+public static class ProcessExtensions
+{
+    public static Task WaitForExitAsync(this Process process)
+    {
+        var tcs = new TaskCompletionSource<object>();
+        process.EnableRaisingEvents = true;
+        process.Exited += (sender, args) => tcs.SetResult(null);
+        if (process.HasExited)
+        {
+            tcs.SetResult(null);
+        }
+        return tcs.Task;
+    }
+}
